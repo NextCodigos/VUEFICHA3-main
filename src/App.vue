65254568a3a73e1f3a074c91915679206e5b91f4
NextCodigos <nebulanemesis372@gmail.com>
2024-03-11 23:59:39 +0100

    Ejercicio Jab estrellas con boton para puntuar estrellas deseadas

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- Componente padre que muestra una lista de artículos -->
+  <div>
+    <!-- Recorre la lista de artículos y envía cada artículo como prop al componente FichaVue -->
+    <div v-for="(cosa, index) in cosas" :key="index">
+      <FichaVue :cosa="cosa" :index="index" @retorno="retorno"/>
+    </div>
+  </div>
+</template>
+  
+<script setup>
+  // Importa el componente FichaVue y la lista de artículos desde datos.js
+  import FichaVue from './components/Ficha.vue'
+  import cosas from "./assets/datos"
+
+  // Función que recibe la puntuación actual y el índice del artículo, y actualiza la puntuación
+  const retorno = (puntuacion, indice) => {
+    if (puntuacion >= 6) {
+      cosas.value[indice].nota = 1; // Reinicia la puntuación si es mayor o igual a 6
+    } else {
+      cosas.value[indice].nota = puntuacion + 1; // Incrementa la puntuación si es menor a 6
+    }
+  }
+</script>
+
+<style scoped></style>
---
+++ src/components/Ficha.vue
@@ -0,0 +1,59 @@
+<template>
+    <!-- Componente que muestra un artículo con su nombre, marca y puntuación -->
+    <div class="display">
+        <button class="boton">
+            {{ cosa.ropa }} ({{ cosa.marca }})
+        </button>
+        <div class="estrellas">
+            <!-- Itera sobre la puntuación y muestra una imagen de estrella por cada punto -->
+            <span v-for="index in cosa.nota" :key="index">
+                <img src="https://www.html6.es/img/estrella.png" alt="Estrella">
+            </span>
+        </div>
+        <!-- Botón para incrementar la puntuación -->
+        <button @click="incrementarNota(cosa.nota,index)">+</button>
+    </div>
+</template>
+
+<script setup>
+const emito = defineEmits('[retorno]');
+const incrementarNota = (puntuacion, indice) => {
+    // Comunica con el componente padre para actualizar la puntuación
+    emito('retorno', puntuacion, indice);
+}
+const props = defineProps({
+    cosa: String, // Propiedad que recibe el nombre, marca y puntuación del artículo
+    index: Number // Propiedad que recibe el índice del artículo en la lista
+});
+</script>
+
+<style scoped>
+/* Estilos para el contenedor principal */
+.display {
+    display: flex; /* Muestra los elementos en línea */
+    align-items: center; /* Alinea verticalmente los elementos */
+}
+
+/* Estilos para las imágenes de estrellas */
+.display img {
+    width: 15px; /* Tamaño de las estrellas */
+    margin: 1px; /* Espacio entre las estrellas */
+}
+
+/* Establece margen a los botones dentro del contenedor principal */
+.display button {
+    margin-right: 20px;
+}
+
+/* Estilos para el botón de artículo */
+.boton {
+    width: 165px; /* Tamaño del botón de artículo */
+}
+
+/* Estilos para el contenedor de estrellas */
+.estrellas {
+    display: flex; /* Muestra las estrellas en línea */
+    align-items: center; /* Alinea verticalmente las estrellas */
+    min-width: 125px; /* Establece un ancho mínimo para el contenedor de estrellas */
+}
+</style>
